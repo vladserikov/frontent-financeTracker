f76fd854f48441dda617bc9c598607f35e6d0d78
Vlad Serikov <vladikserikov@gmail.com>
2024-04-03 23:14:14 +0700

    init project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import User from './user/User';
+
+function App() {
+	return (
+		<>
+			<User />
+		</>
+	);
+}
+
+export default App;
+
---
+++ src/user/User.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+const baseUrl = '/user';
+
+function User() {
+	const [name, setName] = useState('');
+	const [password, setPassword] = useState('');
+
+	const onChangeInput =
+		(setValue: React.Dispatch<React.SetStateAction<string>>) =>
+		(e: React.FormEvent<HTMLInputElement>) => {
+			setValue(e.currentTarget.value);
+		};
+
+	return (
+		<div>
+			<form>
+				<input onChange={onChangeInput(setName)} value={name} />
+				<input onChange={onChangeInput(setPassword)} value={password} />
+				<button type='submit'>login</button>
+			</form>
+		</div>
+	);
+}
+
+export default User;
+
